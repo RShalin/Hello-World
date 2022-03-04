7d30807a51c50584d66dfbce8635cc97b07fa05f
RShalin <63198309+RShalin@users.noreply.github.com>
2022-03-04 10:03:29 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package Test1;
+
+public class HelloWorld {
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World3");
+	}
+}
